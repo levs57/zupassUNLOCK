2695df8b9bbe4be83d273d3951c198c5fe0e8bfb
Lev <0xdeadfae@gmail.com>
2023-11-11 01:51:31 +0300

    bonk

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,38 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import QRCode from "react-qr-code";
+import { RequestInfo } from "undici-types";
+
+type QrResponse = {
+  qr: string,
+}
+
 
 export default function Home() {
+  const [data, setData] = useState<QrResponse>({qr: '420'});
+
+  const getData=async ()=>{
+    await fetch('https://levs57.pythonanywhere.com/get_qr', {
+    mode: 'cors',
+    headers: {
+      'Access-Control-Allow-Origin':'*'
+    }
+  })
+    .then(function(response){
+      console.log(response);
+      return response.json()
+    })
+    .then(function(myJson){
+      console.log(myJson);
+      setData(myJson)
+    })
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
     <div className="flex flex-col justify-center items-center mt-10">
       <div className="flex w-[350px] justify-between items-center">
@@ -22,7 +54,8 @@ export default function Home() {
         <QRCode
           size={320}
           className="mx-auto"
-          value="zupasslengthlengthlengthlengthlengthlengthlengthlengthlengthlengthlengthlengthhlengthlenghlengthlenghlengthleng.org"
+          value={data.qr}
+          //          value="zupasslengthlengthlengthlengthlengthlengthlengthlengthlengthlengthlengthlengthhlengthlenghlengthlenghlengthleng.org"
         />
         <p className="font-extralight font-xs mt-6">Lev, Rafi & Manu</p>
         <p className="font-extralight font-xs mt-2">hack@zuzalu.city</p>
